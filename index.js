62619b150127a4acc41ca4cac06f81e2604f582b
andylwilson <andy787@gmail.com>
2019-05-17 23:27:36 -0700

    Assignment 5 - Database Integration

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
 Andy Wilson / itc230 / spring 2019
-Assignment 3 - Express
+Assignment 5 - Database Integration
 */
 
 'use strict'
@@ -8,6 +8,8 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const app = express();
 const books = require('./lib/books_model.js'); //import books module to use its data and methods
+const bookMethods = require("./lib/bookMethods.js"); //import methods to access database
+const bookDB = require("./models/Book"); //import DB model
 
 let handlebars =  require("express-handlebars");
 app.engine(".html", handlebars({extname: '.html'}));
@@ -17,47 +19,45 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
 
- // send content of 'home' view
-app.get('/', (req,res) => {
-  res.render('home', {library: books.getAll()});
- });
+app.get('/', (req, res, next) => {
+  bookMethods.getAll().then((items) => {
+    res.render('home', {library: items }); 
+  }).catch((err) =>{
+    return next(err);
+  });
+});
 
- app.get('/about', (req, res) => {
-  res.type('text/html');
-  res.sendFile(__dirname + '/public/about.html'); 
- });
+ app.post('/get', (req, res, next) => {
+  bookMethods.getOne(req.body.title).then((item) => {
+    res.render('details', {'book': item }); 
+  }).catch((err) =>{
+    return next(err);
+  });
+});
 
- app.get('/details', (req, res) => {
-  res.type('text/html');
-  res.render('details');
- });
+app.get('/get', (req, res, next) => {
+  bookMethods.getOne(req.query.title).then((item) => {
+    res.render('details', {'book': item }); 
+  }).catch((err) =>{
+    return next(err);
+  });
+});
 
- app.post('/get', (req,res) => {
-  let result = books.get(req.body.title);
-  res.render('details', {title: req.body.title, result: result });
- });
+app.get('/delete', (req, res, next) => {
+  bookMethods.delete(req.query.title).then(() => {
+    res.render('delete', {book: req.query.title}); 
+  }).catch((err) =>{
+    return next(err);
+  });
+});
 
-app.get('/getall', (req,res) => {
-  let result = books.getAll();
-  res.render('details', {allbooks: result});
- });
-
-app.get('/add', (req,res) => {
-  let newBook = {title: "Calypso", author: "David Sedaris", isbn:"003163"};
-  let result = books.add(newBook);
-  res.render('details', {newlibrary: JSON.stringify(result)});
- });
-
-// add method using query string
-//  app.get('/add', (req,res) => {
-//   let result = books.add(req.query.title, req.query.author, req.query.isbn);
-//   res.render('details', {title: req.query.title, newlibrary: result});
-//  });
-
-app.get('/delete', (req,res) => {
-  let result = books.delete(req.query.title);
-  res.render('delete', {title: req.query.title, result: result});
- });
+app.post('/add', (req, res, next) => {
+  bookMethods.addOne(req.body.addTitle,req.body.addAuthor,req.body.addISBN).then((item) => {
+    res.render('add', {book : item}); 
+  }).catch((err) =>{
+    return next(err);
+  });
+});
 
  // define 404 handler
  app.use( (req,res) => {
--- models/Book.js
+++ models/Book.js
@@ -1,13 +1,13 @@
-const mongoose = require(‘mongoose’);
+const mongoose = require('mongoose');
 
 // remote db connection settings. For security, connectionString should be in a separate file not committed to git
-const credentials = require('../config/custom_config');
+const credentials = require('../config/db_config');
 
 // local db connection settings 
 // const ip = process.env.ip || '127.0.0.1';
 // const connectionString = 'mongodb://' +ip+ '/<DB_NAME>';
 
-mongoose.connect(credentials.connection(), { dbName: <dbname>, useNewUrlParser: true }); 
+mongoose.connect(credentials.connection(), { dbName: 'projects', useNewUrlParser: true }); 
 
 mongoose.connection.on('open', () => {
   console.log('Mongoose connected.');
@@ -18,9 +18,7 @@ mongoose.connection.on('open', () => {
 const mySchema = mongoose.Schema({
  title: { type: String, required: true },
  author: String,
- count: Number,
- pubdate: Date,
- inStore: Boolean
+ isbn: Number,
 }); 
 
-module.exports = mongoose.model('Book', mySchema);
+module.exports = mongoose.model('books', mySchema);

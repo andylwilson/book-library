fc4e48de941332f620890ef0040620ec34b9008f
andylwilson <andy787@gmail.com>
2019-06-14 21:22:28 -0700

    Assignment 6 - REST APIs

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 /*
 Andy Wilson / itc230 / spring 2019
-Assignment 5 - Database Integration
+Assignment 6 - REST APIs
 */
 
 'use strict'
 const express = require("express");
 const bodyParser = require("body-parser")
 const app = express();
-const books = require('./lib/books_model.js'); //import books module to use its data and methods
 const bookMethods = require("./lib/bookMethods.js"); //import methods to access database
-const bookDB = require("./models/Book"); //import DB model
+//const routes = require('./routes.js')(app);
 
 let handlebars =  require("express-handlebars");
 app.engine(".html", handlebars({extname: '.html'}));
@@ -18,7 +17,11 @@ app.set("view engine", ".html");
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
+app.use('/api', require('cors')()); // set Access-Control-Allow-Origin header for api route
 
+// *** standard routes ***
+
+// home
 app.get('/', (req, res, next) => {
   bookMethods.getAll().then((items) => {
     res.render('home', {library: items }); 
@@ -27,7 +30,8 @@ app.get('/', (req, res, next) => {
   });
 });
 
- app.post('/get', (req, res, next) => {
+// get one POST
+app.post('/get', (req, res, next) => {
   bookMethods.getOne(req.body.title).then((item) => {
     res.render('details', {'book': item }); 
   }).catch((err) =>{
@@ -35,6 +39,7 @@ app.get('/', (req, res, next) => {
   });
 });
 
+// get one GET
 app.get('/get', (req, res, next) => {
   bookMethods.getOne(req.query.title).then((item) => {
     res.render('details', {'book': item }); 
@@ -43,6 +48,7 @@ app.get('/get', (req, res, next) => {
   });
 });
 
+// delete
 app.get('/delete', (req, res, next) => {
   bookMethods.delete(req.query.title).then(() => {
     res.render('delete', {book: req.query.title}); 
@@ -51,6 +57,7 @@ app.get('/delete', (req, res, next) => {
   });
 });
 
+// add
 app.post('/add', (req, res, next) => {
   bookMethods.addOne(req.body.addTitle,req.body.addAuthor,req.body.addISBN).then((item) => {
     res.render('add', {book : item}); 
@@ -59,13 +66,54 @@ app.post('/add', (req, res, next) => {
   });
 });
 
- // define 404 handler
- app.use( (req,res) => {
+// *** api routes ***
+
+// get all
+app.get('/api/books', (req, res, next) => {
+  bookMethods.getAll().then((items) => {
+    res.json(items); 
+  }).catch((err) =>{
+    return next(err);
+    });
+});
+
+// get one
+app.get('/api/book/:title', (req, res, next) => {
+  let title = req.params.title;
+  bookMethods.getOne(title).then((item) => {
+    res.json(item); 
+  }).catch((err) =>{
+    return next(err);
+  });
+});
+
+// delete
+app.get('/api/book/delete/:title', (req, res, next) => {
+  let title = req.params.title;
+  bookMethods.delete(title).then(() => {
+   // let books = bookMethods.getAll();
+    res.json({'result' : 'deleted'});
+  }).catch((err) =>{
+    return next(err);
+  });
+});
+
+// add
+app.post('/api/add', (req, res, next) => {
+  bookMethods.addOne(req.body.addTitle,req.body.addAuthor,req.body.addISBN).then(() => {
+    res.json({'result' : 'added / updated'}); 
+  }).catch((err) =>{
+    return next(err);
+  });
+});
+
+// define 404 handler
+app.use( (req,res) => {
   res.type('text/plain'); 
   res.status(404);
   res.send('404 - Not found');
- });
+});
 
 app.listen(app.get('port'), () => {
   console.log('Express started'); 
- });
+});
